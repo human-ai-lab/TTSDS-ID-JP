dd7f5b06fe99b069ed9e22222face0c1524f0e69
Michael <86698712+michaelbrian02@users.noreply.github.com>
2025-08-29 21:13:32 +0900

    Update intialize.bat

--- intialize.bat
+++ intialize.bat
@@ -1,5 +1,5 @@
 @echo off
-python initialize.py
+python initialize.py "false"
 if errorlevel 1 (
     echo Error occurred during download!
     pause
@@ -7,3 +7,4 @@ if errorlevel 1 (
     echo Download completed successfully!
 )
 pause
+
